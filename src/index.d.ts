bb9055124e7ce11527ec34952d7c5ce030765ffc
Jimin Park <pjm950@gmail.com>
2026-02-12 15:42:05 +0900

    Added type information.

---
+++ src/index.d.ts
@@ -0,0 +1,9 @@
+import type {Linter} from "eslint";
+
+export declare const strict: Linter.Config[];
+export declare const style: Linter.Config[];
+export declare const javascript: Linter.Config[];
+export declare const typescript: Linter.Config[];
+
+declare const _default: Linter.Config[];
+export default _default;
